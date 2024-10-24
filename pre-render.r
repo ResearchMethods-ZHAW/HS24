399aa788e1a7934ad16f72134fcc5ef7da4ac93c
ratnanil <rata@zhaw.ch>
2024-10-24 12:48:57 +0200

    rm messages

--- pre-render.r
+++ pre-render.r
@@ -1,7 +1,11 @@
 
 if(!dir.exists("purl")){dir.create("purl")}
-list.files(path = c("statistik"), pattern = "\\.qmd$", recursive = TRUE, full.names = TRUE) |>
-    sapply(\(x){
+
+qmd_files <- list.files(path = c("statistik"), pattern = "\\.qmd$", recursive = TRUE, full.names = TRUE)
+
+sapply(qmd_files, \(x){
         y <- file.path("purl", paste0(tools::file_path_sans_ext(basename(x)), ".R"))
-        knitr::purl(x, output = y, documentation = 2L)
-    })
+        knitr::purl(x, output = y, documentation = 2L, quiet = TRUE)
+        
+    }) |> 
+  invisible()
