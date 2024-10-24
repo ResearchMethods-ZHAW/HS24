9c05a4c0300972efba806c0b97841227903ddde7
ratnanil <rata@zhaw.ch>
2024-10-24 12:33:37 +0200

    fix issue

--- pre-render.r
+++ pre-render.r
@@ -1,3 +1,5 @@
+
+if(!dir.exists("purl")){dir.create("purl")}
 list.files(path = c("statistik"), pattern = "\\.qmd$", recursive = TRUE, full.names = TRUE) |>
     sapply(\(x){
         y <- file.path("purl", paste0(tools::file_path_sans_ext(basename(x)), ".R"))
